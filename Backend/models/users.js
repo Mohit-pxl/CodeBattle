6d05a0851353749a0bf26132ca5e8a2fdb1a9e03
Mohit <mohitydv1979@gmail.com>
2025-12-08 07:52:13 +0530

    user schema

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -3,8 +3,42 @@ const {Schema}=mongoose
 
 const UserSchema=new Schema({
     firstName:{
+        type:String,
+        require:true,
+        minLength:3,
+        maxLength:20
+    },
+    lastName:{
+        type:String,
+        minLength:3,
+        maxLength:20,
+
+    },
+    emailId:{
+        type:string,
+        require:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        immutable:true
+    },
+    age:{
+        type:Number,
+        min:6,
+        max:80,
+    },
+    role:{
+        type:string,
+        enum:["user","admin"],
+        default:"user"
+
+    },
+    problemSolved:{
+        type:[string]
 
     }
+},{
+    timestamps:true
 })
 
 const User=mongoose.model("User",UserSchema)
